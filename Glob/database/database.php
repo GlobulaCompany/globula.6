914eec4e067d288ffdbbc85faa451df8ec97213c
Globula <111533456+GlobulaCompany@users.noreply.github.com>
2022-09-03 19:25:44 +0300

    Update database.php

--- Glob/database/database.php
+++ Glob/database/database.php
@@ -1,26 +1,18 @@
 <?php
 
-// $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-// $server = $url["host"];
-// $username = $url["user"];
-// $password = $url["pass"];
-// $db = substr($url["path"], 1);
-// date_default_timezone_set('Africa/Nairobi');
-// $conn = new mysqli($server, $username, $password, $db);
-
-
-
-
-
-$servername = "localhost";
-$username = "root";
-$password = "";
-$db ="globula";
-
-// Create connection
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
 date_default_timezone_set('Africa/Nairobi');
-$conn = new mysqli($servername, $username, $password,$db);
+$conn = new mysqli($server, $username, $password, $db);
+
+
+
+
+ 
 
 
 ?>
