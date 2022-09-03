6aad9a59ed3695bf21455d36495124438e983179
Globula <111533456+GlobulaCompany@users.noreply.github.com>
2022-09-03 19:26:08 +0300

    Update database_connection.php

--- Glob/database/database_connection.php
+++ Glob/database/database_connection.php
@@ -1,25 +1,18 @@
 <?php
  
 
-//  $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+ $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-// $server = $url["host"];
-// $username = $url["user"];
-// $password = $url["pass"];
-// $db = substr($url["path"], 1);
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
 
  
  
-// date_default_timezone_set('Africa/Nairobi');
+date_default_timezone_set('Africa/Nairobi');
 
-// $connect = new PDO("mysql:host=$server;dbname=$db;charset=utf8mb4", "$username", "$password");
-
-
-
-
-date_default_timezone_set('Africa/Nairobi'); 
-
-$connect = new PDO("mysql:host=localhost;dbname=globula;charset=utf8mb4", "root", "");
+$connect = new PDO("mysql:host=$server;dbname=$db;charset=utf8mb4", "$username", "$password");
 
 
 
